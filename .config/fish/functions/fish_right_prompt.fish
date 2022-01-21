84ca81413748cf3a7fcb63ca546a459a5c8b1593
Julia <julaschmid@gmail.com>
2022-01-21 17:21:30 +0100

    fix fish prompt

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -2,7 +2,7 @@ function fish_right_prompt
   set_color $fish_color_autosuggestion 2> /dev/null; or set_color 555
   date "+%H:%M:%S" 
   echo @ 
-  if test (hostname) = "Julias-MBP-5"
+  if test (hostname) = "Julias-MacBook-Pro.local"
     echo "local"
   else
     hostname
